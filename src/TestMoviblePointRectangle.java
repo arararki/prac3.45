b0c15dedec48e29809e018d2c208aa8f4d2b6768
arararki <ueuk332ctvm>
2020-09-16 23:57:16 +0300

    Initial commit

---
+++ src/TestMoviblePointRectangle.java
@@ -0,0 +1,11 @@
+public class TestMoviblePointRectangle {
+    public TestMoviblePointRectangle() {
+    }
+
+    public static void main(String[] args) {
+        MoviblePointRectangle c1 = new MoviblePointRectangle(-3, 2, 3, -2, 3, 3);
+        System.out.println(c1);
+        c1.moveUp();
+        System.out.println(c1);
+    }
+}
